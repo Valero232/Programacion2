705f8152a45cd74c2c0f598416b97c7f1de140ce
aaron <aaronrodriguezraya@gmail.com>
2026-02-24 20:59:35 +0100

    Clase reserva

---
+++ src/prog2/model/Reserva.java
@@ -0,0 +1,57 @@
+package prog2.model;
+
+import java.time.LocalDate;
+
+public class Reserva implements InReserva{
+
+    //Atributs
+    private Client client;
+    private Allotjament allotjament;
+    private LocalDate dataEntrada;
+    private LocalDate dataSortida;
+
+    public Reserva(Client client, Allotjament allotjament, LocalDate diaEntrada, LocalDate diaSortida){
+
+    }
+
+    @Override
+    public Allotjament getAllotjament_() {
+        return this.allotjament;
+    }
+
+    @Override
+    public Client getClient() {
+        return this.client;
+    }
+
+    @Override
+    public LocalDate getDataEntrada() {
+        return this.dataEntrada;
+    }
+
+    @Override
+    public LocalDate getDataSortida() {
+        return this.dataSortida;
+    }
+
+    @Override
+    public void setAllotjament_(Allotjament allotjament_) {
+        this.allotjament = allotjament_;
+    }
+
+    @Override
+    public void setClient(Client client_) {
+        this.client = client_;
+    }
+
+    @Override
+    public void setDataEntrada(LocalDate dataEntrada_) {
+        this.dataEntrada = dataEntrada_;
+
+    }
+
+    @Override
+    public void setDataSortida(LocalDate dataSortida_) {
+        this.dataSortida = dataSortida_;
+    }
+}
